803a0883b8679803d3e8b5739b87c4ce50dfa2e6
Vaishali Sharma <33980820+vaishali161197@users.noreply.github.com>
2021-04-13 00:38:20 +0530

    Add Zucchini

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    bool spawn = true;
+    [SerializeField] float minDelay = 1f;
+    [SerializeField] float maxDelay = 5f;
+    [SerializeField] Attacker attackerPrefab;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while(spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+            SpawnAttacker();
+        }
+       
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate( attackerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    [SerializeField] int TimeToWait = 4;
+    int CurrentSceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        StartCoroutine(WaitForTime());
+    }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(TimeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(CurrentSceneIndex + 1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
